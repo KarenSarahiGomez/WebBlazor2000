79d8db647da0fc7a309fc17e7bb7aa25640223c2
Karen Gómez <ksgomezg@unah.hn>
2023-04-18 19:58:28 -0600

    Codificaación de los EditarUsuario.razors
    Clase 5 del tercer parcial

--- Web/Blazor/Pages/MisUsuarios/EditarUsuario.razor.cs
+++ Web/Blazor/Pages/MisUsuarios/EditarUsuario.razor.cs
@@ -1,7 +1,22 @@
-﻿namespace Blazor.Pages.MisUsuarios
+﻿using Blazor.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Blazor.Pages.MisUsuarios
 {
     public partial class EditarUsuario
     {
+        [Inject] private IUsuarioServicio usuarioServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        private Usuario user = new Usuario();
+        [Parameter] public string CodigoUsuario { get; set; }
 
+        protected override async Task OnInitializedAsync()
+        {
+            if (!string.IsNullOrEmpty(CodigoUsuario))
+            {
+                user = await usuarioServicio.GetPorCodigoAsync(CodigoUsuario);
+            }
+        }
     }
 }
--- Web/Datos/Interfaces/IUsuarioRepositorio.cs
+++ Web/Datos/Interfaces/IUsuarioRepositorio.cs
@@ -5,7 +5,7 @@ namespace Datos.Interfaces
     public interface IUsuarioRepositorio
     {
         //declaramos metodos para poder manipular la tabla de usuarios
-        Task<Usuario> GetPorCodigoAsync(string codigo);
+        Task<Usuario> GetPorCodigoAsync(string codigoUsuario);
         Task<bool> NuevoAsync(Usuario usuario);
         Task<bool> ActualizarAsync(Usuario usuario);
         Task<bool> EliminarAsync(string codigo);
